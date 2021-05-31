1d502ba36e25e13d2bb25ecf3c60f654d708a318
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-31 08:23:08 -0300

    Comandos del Aula 2

--- comandos.sql
+++ comandos.sql
@@ -3,3 +3,11 @@
 Por el momento, te invitamos a preparar el ambiente para que puedas desarrollar todos los ejercicios. 
 
 ¡Te deseo muchos éxitos en tus estudios!*/
+
+/*COMANDOS AULA 1
+
+A partir de este punto, estás listo para trabajar directamente en Workbench.
+
+Debes ver todos los videos referentes al Aula 2 y colocar en práctica lo que estarás aprendiendo.
+
+¡Mucho ánimo! Nuestro curso ha comenzado.*/
