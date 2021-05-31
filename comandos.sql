f6141888c7ab0eeb3dea598b2c70f8f9a3bc2a47
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-31 08:10:49 -0300

    Comandos del Aula 1

---
+++ comandos.sql
@@ -0,0 +1,5 @@
+/*En este archivo .sql encontrarás todos los comandos que ejecutaremos durante el desarrollo de nuestro entrenamiento. 
+
+Por el momento, te invitamos a preparar el ambiente para que puedas desarrollar todos los ejercicios. 
+
+¡Te deseo muchos éxitos en tus estudios!*/
